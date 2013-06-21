dfa6c6e642c8382e7deb55497afed1cb1d618298
sephthir <sephthir@gmail.com>
2013-06-20 22:08:45 -0600

    Fixed whitespace/indentation and brackets to be consistent.

--- mods/sephthir/client/renderer/ItemRendererEnderBooks.java
+++ mods/sephthir/client/renderer/ItemRendererEnderBooks.java
@@ -26,50 +26,53 @@ import net.minecraftforge.client.IItemRenderer;
 public class ItemRendererEnderBooks implements IItemRenderer {
 
 	@Override
-	public boolean handleRenderType(ItemStack item, ItemRenderType type) {
+	public boolean handleRenderType(ItemStack item, ItemRenderType type)
+	{
 		return type == ItemRenderType.INVENTORY;
 	}
 
 	@Override
 	public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item,
-			ItemRendererHelper helper) {
+			ItemRendererHelper helper)
+	{
 		return false;
 	}
 
 	@Override
-	public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
+	public void renderItem(ItemRenderType type, ItemStack item, Object... data)
+	{
 		RenderEngine engine = Minecraft.getMinecraft().renderEngine;
 		Icon icon = item.getItem().getIcon(item, 0);
 		
 		GL11.glDisable(GL11.GL_LIGHTING);
 
-        if (item.getItemSpriteNumber() == 0)
-        {
-            engine.bindTexture("/terrain.png");
-        }
-        else
-        {
-            engine.bindTexture("/gui/items.png");
-        }
+		if (item.getItemSpriteNumber() == 0)
+		{
+		    engine.bindTexture("/terrain.png");
+		}
+		else
+		{
+		    engine.bindTexture("/gui/items.png");
+		}
 
-        if (icon == null)
-        {
-            icon = engine.getMissingIcon(item.getItemSpriteNumber());
-        }
+		if (icon == null)
+		{
+		    icon = engine.getMissingIcon(item.getItemSpriteNumber());
+		}
 
-        this.renderIcon(0, 0, icon, 16, 16);
-        GL11.glEnable(GL11.GL_LIGHTING);
+		this.renderIcon(0, 0, icon, 16, 16);
+		GL11.glEnable(GL11.GL_LIGHTING);
 	}
 	
 	public void renderIcon(int par1, int par2, Icon par3Icon, int par4, int par5)
-    {
-        Tessellator tessellator = Tessellator.instance;
-        tessellator.startDrawingQuads();
-        tessellator.addVertexWithUV((double)(par1 + 0), (double)(par2 + par5), 0, (double)par3Icon.getMinU(), (double)par3Icon.getMaxV());
-        tessellator.addVertexWithUV((double)(par1 + par4), (double)(par2 + par5), 0, (double)par3Icon.getMaxU(), (double)par3Icon.getMaxV());
-        tessellator.addVertexWithUV((double)(par1 + par4), (double)(par2 + 0), 0, (double)par3Icon.getMaxU(), (double)par3Icon.getMinV());
-        tessellator.addVertexWithUV((double)(par1 + 0), (double)(par2 + 0), 0, (double)par3Icon.getMinU(), (double)par3Icon.getMinV());
-        tessellator.draw();
-    }
+	{
+		Tessellator tessellator = Tessellator.instance;
+		tessellator.startDrawingQuads();
+		tessellator.addVertexWithUV((double)(par1 + 0), (double)(par2 + par5), 0, (double)par3Icon.getMinU(), (double)par3Icon.getMaxV());
+		tessellator.addVertexWithUV((double)(par1 + par4), (double)(par2 + par5), 0, (double)par3Icon.getMaxU(), (double)par3Icon.getMaxV());
+		tessellator.addVertexWithUV((double)(par1 + par4), (double)(par2 + 0), 0, (double)par3Icon.getMaxU(), (double)par3Icon.getMinV());
+		tessellator.addVertexWithUV((double)(par1 + 0), (double)(par2 + 0), 0, (double)par3Icon.getMinU(), (double)par3Icon.getMinV());
+		tessellator.draw();
+	}
 
 }
